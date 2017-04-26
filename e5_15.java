208eda5cad8c98c7062eaecd037f0296a3e74c84
Manar Mohammed <mkmanar762@gmail.com>
2017-04-26 00:18:27 -0700

    Update e5_15.java

--- e5_15.java
+++ e5_15.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 
 public class e5_15 {
+     /**
+    The following algorithm yields the season (Spring, Summer, Fall, or Winter) for a given month and day
+    * . If month is 1, 2, or 3, season = “Winter” 
+    * Else if month is 4, 5, or 6, season = “Spring” 
+    * Else if month is 7, 8, or 9, season = “Summer” 
+    * Else if month is 10, 11, or 12, season = “Fall” 
+    * If month is divisible by 3 and day >= 21 If season is “Winter”, season = “Spring”
+    * Else if season is “Spring”, season = “Summer”  Else if season is “Summer”, season = “Fall” 
+    * Else season = “Winter”
+    * Write a program that prompts the user for a month and day and then prints the season,
+    * as determined by this algorithm. Use a class Date with a method getSeason. 
+     */
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int month;
